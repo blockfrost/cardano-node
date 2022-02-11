ca9d8fb3df02e1b8287f4225056ed0210df2165c
Denis Shevchenko <denis.shevchenko@iohk.io>
2022-02-11 13:34:54 +0400

    cardano-tracer: sequential tests.

--- cardano-tracer/test/cardano-tracer-test.hs
+++ cardano-tracer/test/cardano-tracer-test.hs
@@ -1,3 +1,4 @@
+import           System.Environment (setEnv)
 import           Test.Tasty
 
 import qualified Cardano.Tracer.Test.Logs.Tests as Logs
@@ -6,8 +7,9 @@ import qualified Cardano.Tracer.Test.Restart.Tests as Restart
 import qualified Cardano.Tracer.Test.Queue.Tests as Queue
 
 main :: IO ()
-main = defaultMain $
-  testGroup "cardano-tracer"
+main = do
+  setEnv "TASTY_NUM_THREADS" "1" -- For sequential running of tests.
+  defaultMain $ testGroup "cardano-tracer"
     [ Logs.tests
     , DataPoint.tests
     , Restart.tests
